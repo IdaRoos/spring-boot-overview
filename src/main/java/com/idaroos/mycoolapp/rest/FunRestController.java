d39d0caa428862a6f79b44de7d31addca84fb5be
IdaRoos <ida_r@live.se>
2023-06-05 15:11:27 +0200

    Spring boot overview

--- src/main/java/com/idaroos/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/idaroos/mycoolapp/rest/FunRestController.java
@@ -1,2 +1,15 @@
-package com.idaroos.mycoolapp.rest;public class FunRestController {
+package com.idaroos.mycoolapp.rest;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // expose "/" that return "Hello World"
+    @GetMapping("/")
+    public String sayHello() {
+       return "Hello World!";
+    }
 }
